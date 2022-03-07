ac7f1236c5fba7b858daea1e5fde3d23f4a80370
kimulia1 <ekzit96@gmail.ru>
2022-03-07 07:55:48 +0700

    fix action 2

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
-export const greetingUser = () => {
-  const userName = readlineSync.question("May I have your name? ");
+const greetingUser = () => {
+  const userName = readlineSync.question('May I have your name? ');
   return console.log(`Hello, ${userName}`);
 };
+
+export default greetingUser;
