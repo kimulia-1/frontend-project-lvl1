66a82957ef3260b3be790f51adf8a1052650c733
kimulia1 <ekzit96@gmail.ru>
2022-06-16 10:20:20 +0700

    fix project 2

--- src/games/primeGame.js
+++ src/games/primeGame.js
@@ -1,12 +1,16 @@
 export const primeGame = () => {
-  let result = 'yes';
+  let result = '';
   const num = Math.floor(Math.random() * 10) + 1;
   console.log(`Question: ${num}`);
+  if (num === 1) {
+    result = 'no';
+  }
   for (let div = 2; div < num; div += 1) {
     if (num % div === 0) {
       result = 'no';
       break;
     }
+    result = 'yes';
   }
   return result;
 };
